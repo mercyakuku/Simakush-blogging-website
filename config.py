d0e1e39e80b8e9513665f7cc4f2683e7bd2ffbaf
mercyakuku <mercyakuku24@gmail.com>
2022-05-20 09:03:39 +0300

    Deployment

--- config.py
+++ config.py
@@ -7,6 +7,8 @@ class Config:
 
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://grace:simakush@localhost/simablog'
 
+    SQLALCHEMY_DATABASE_URI = 'postgres://knlzjntoipebhz:df79385a36c28d2b94247f41c7925dd7e1779d8c5a7c0c3d8cadd74315f31888@ec2-3-231-82-226.compute-1.amazonaws.com:5432/dd49uubfm86359'
+
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
 
     SIMPLEMDE_JS_IIFE = True
